39b8809eb8848011c601693a18ccd2cc26f55080
anemiaa <anemiaa1986@gmail.com>
2023-01-19 01:42:07 +0300

    spr6.8

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.core.paginator import Paginator
 from .forms import CommentForm, PostForm
 from .models import Follow, Group, Post, User
 from .utils import get_page_pagi_func
