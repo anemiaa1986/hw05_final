91205682c3d0a567173aef7a5c997870ddbf71bf
anemiaa <anemiaa1986@gmail.com>
2023-01-20 03:07:58 +0300

    spr6 done

--- yatube/posts/utils.py
+++ yatube/posts/utils.py
@@ -1,7 +1,11 @@
 from django.core.paginator import Paginator
+from typing import Any
 
 
-def get_page_pagi_func(request, object, magic_numbers):
-    paginator = Paginator(object, magic_numbers)
+def get_page_pagi_func(request, queryset, posts_on_page) -> Any:
+    """
+    функция пагинации для views, вынесена в отдельный модуль
+    """
+    paginator = Paginator(queryset, posts_on_page)
     page_number = request.GET.get('page')
     return paginator.get_page(page_number)
