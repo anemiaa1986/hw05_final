ef5a40424363cd1e7277eedef812556828981cf4
anemiaa <anemiaa1986@gmail.com>
2023-01-21 16:36:39 +0300

    spr6.9

--- yatube/posts/utils.py
+++ yatube/posts/utils.py
@@ -1,8 +1,13 @@
-from django.core.paginator import Paginator
+from django.core.paginator import Page, Paginator
 from django.db.models import QuerySet
+from django.core.handlers.wsgi import WSGIRequest
 
 
-def get_page_pagi_func(request, objects, posts_on_page) -> QuerySet:
+def get_page_pagi_func(
+    request: WSGIRequest,
+    objects: QuerySet,
+    posts_on_page: int
+        ) -> Page:
     """
     функция пагинации для views, вынесена в отдельный модуль
     """
